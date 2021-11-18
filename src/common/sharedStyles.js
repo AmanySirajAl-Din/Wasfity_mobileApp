e870890e5f182ffce1bf88c1520ad08969585a19
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-11-18 11:59:25 +0200

    Create common shared styles

---
+++ src/common/sharedStyles.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+import {COLORS, FONTS} from './';
+
+export default StyleSheet.create({
+  w100: {
+    width: '100%',
+  },
+  h100: {
+    height: '100%',
+  },
+  center: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mainFont: {
+    fontFamily: FONTS.regular,
+  },
+  mainFontBold: {
+    fontFamily: FONTS.bold,
+  },
+});
