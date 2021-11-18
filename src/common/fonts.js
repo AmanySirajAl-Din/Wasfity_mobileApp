cc21cfd1de8d51a5cada591d87637d9338535890
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-11-18 11:50:41 +0200

    Add common main font 'Roboto'

---
+++ src/common/fonts.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+const FONTS = {
+  // const must be same name at export
+  regular: 'Roboto-Regular',
+  medium: 'Roboto-Medium',
+  bold: 'Roboto-Bold',
+  italic: 'Roboto-Italic',
+  thin: 'Roboto-Thin',
+  light: 'Roboto-Light',
+};
+export {FONTS}; // export as name module
