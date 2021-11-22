6bed075375cce363c7dc0457501c4f370a0ab347
Amany Siraj Al-Din <amanysirajaldin@gmail.com>
2021-11-22 21:16:09 +0200

    Convert 'IntroScreens' to function cause we need to write js code

--- src/screens/Intro3/index.js
+++ src/screens/Intro3/index.js
@@ -8,50 +8,54 @@ import styles from './styles';
 import mainBg from '../../../assets/images/Background.png';
 import introEffect from '../../../assets/images/Effect4.png';
 
-export const IntroScreens = () => (
-  <ImageBackground
-    source={mainBg}
-    style={[SharedStyles.flex1]}
-    imageStyle={{
-      resizeMode: 'cover',
-      alignSelf: 'flex-start',
-    }}>
+export function IntroScreens() {
+  
+
+  return (
     <ImageBackground
-      source={introEffect}
-      resizeMode="contain"
-      style={[SharedStyles.flex2]}>
-      <Onboarding
-        onSkip={() => {}}
-        onDone={() => {}}
-        titleStyles={styles.title}
-        subTitleStyles={styles.text}
-        pages={[
-          {
-            backgroundColor: '#ffffff00',
-            image: (
-              <Image source={require('../../../assets/images/food1.png')} />
-            ),
-            title: 'Delicious Food',
-            subtitle: 'Let’s eat some healthy food while steak to cook',
-          },
-          {
-            backgroundColor: '#ffffff00',
-            image: (
-              <Image source={require('../../../assets/images/food2.png')} />
-            ),
-            title: 'Healthy & Tasty',
-            subtitle: 'Eat today live another memorable day',
-          },
-          {
-            backgroundColor: '#ffffff00',
-            image: (
-              <Image source={require('../../../assets/images/food3.png')} />
-            ),
-            title: 'Let’s Eat',
-            subtitle: 'Food is really and truly the most effective medicine',
-          },
-        ]}
-      />
+      source={mainBg}
+      style={[SharedStyles.flex1]}
+      imageStyle={{
+        resizeMode: 'cover',
+        alignSelf: 'flex-start',
+      }}>
+      <ImageBackground
+        source={introEffect}
+        resizeMode="contain"
+        style={[SharedStyles.flex2]}>
+        <Onboarding
+          onSkip={() => {}}
+          onDone={() => {}}
+          titleStyles={styles.title}
+          subTitleStyles={styles.text}
+          pages={[
+            {
+              backgroundColor: '#ffffff00',
+              image: (
+                <Image source={require('../../../assets/images/food1.png')} />
+              ),
+              title: 'Delicious Food',
+              subtitle: 'Let’s eat some healthy food while steak to cook',
+            },
+            {
+              backgroundColor: '#ffffff00',
+              image: (
+                <Image source={require('../../../assets/images/food2.png')} />
+              ),
+              title: 'Healthy & Tasty',
+              subtitle: 'Eat today live another memorable day',
+            },
+            {
+              backgroundColor: '#ffffff00',
+              image: (
+                <Image source={require('../../../assets/images/food3.png')} />
+              ),
+              title: 'Let’s Eat',
+              subtitle: 'Food is really and truly the most effective medicine',
+            },
+          ]}
+        />
+      </ImageBackground>
     </ImageBackground>
-  </ImageBackground>
-);
+  );
+}
