7d1b872abf25389ec9646678d1b7b3a8d591e4f1
Amany Siraj Al-Din <amanysirajaldin@gmail.com>
2021-11-22 23:56:21 +0200

    Add comments for importing and exporting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+
+// import {} this is a named component
 import {NavigationContainer} from '@react-navigation/native';
 import {
   SafeAreaView,
@@ -8,6 +10,7 @@ import {
   ImageBackground,
 } from 'react-native';
 
+// import without {} this is a default exported component
 import AppContainer from './navigation';
 
 const App = () => {
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,4 +1,5 @@
 export {default as Intro} from './Intro';
+// export default as Intro >> can only use the name Intro to import Intro component
 export {Intro2} from './Intro2';
 export {IntroScreens} from './Intro3';
 export {default as HomeScreen} from './Home';
