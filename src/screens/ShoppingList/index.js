f9b2a72b4ef8f20cbd93b3b199888a17335eb46b
Amany Siraj Al-Din <amanysirajaldin@gmail.com>
2021-11-27 09:08:12 +0200

    Update Shopping List Style

--- src/screens/ShoppingList/index.js
+++ src/screens/ShoppingList/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, TextInput, View} from 'react-native';
+import {SharedStyles} from '../../common';
 
 import styles from './styles';
 
 export const ShoppingList = () => {
-  return <Text style={styles.text}>Hello ShoppingList</Text>;
+  return (
+    <View style={[SharedStyles.h100, SharedStyles.center, styles.mainView]}>
+      <Text style={styles.text}>Your Shopping List is Empty</Text>
+    </View>
+  );
 };
--- src/screens/ShoppingList/styles.js
+++ src/screens/ShoppingList/styles.js
@@ -2,11 +2,16 @@ import {StyleSheet} from 'react-native';
 import {COLORS, FONTS} from '../../common';
 
 export default StyleSheet.create({
+  mainView: {
+    padding: 10,
+  },
   text: {
     color: COLORS.black,
     fontFamily: FONTS.regular,
     fontFamily: FONTS.bold,
     fontSize: 24,
     lineHeight: 25,
+    textAlign: 'center',
+    lineHeight: 40,
   },
 });
